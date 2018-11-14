1efbc731b979640b5fdd69b468ec26305b40473c
Mark Davies <mark.davies@esendex.com>
2018-11-14 13:20:09 +0000

    Copying gilded rose kata - will write tests soon

---
+++ src/Refactor.Kata/Refactor.Kata/GildedRose.cs
@@ -0,0 +1,91 @@
+﻿namespace Refactor.Kata
+{
+    public class GildedRose
+    {
+        public string Name { get; }
+
+        public int SellIn { get; private set; }
+
+        public int Quality { get; private set; }
+
+        public GildedRose(string name, int sellIn, int quality)
+        {
+            Name = name;
+            SellIn = sellIn;
+            Quality = quality;
+        }
+
+        private void Tick(string[] args)
+        {
+            if (Name != "Aged Brie" && Name != "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (Quality > 0)
+                {
+                    if (Name != "Sulfuras, Hand of Ragnaros")
+                    {
+                        Quality = Quality - 1;
+                    }
+                }
+            }
+            else
+            {
+                if (Quality < 50)
+                {
+                    Quality = Quality + 1;
+
+                    if (Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (SellIn < 11)
+                        {
+                            if (Quality < 50)
+                            {
+                                Quality = Quality + 1;
+                            }
+                        }
+
+                        if (SellIn < 6)
+                        {
+                            if (Quality < 50)
+                            {
+                                Quality = Quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (Name != "Sulfuras, Hand of Ragnaros")
+            {
+                SellIn = SellIn - 1;
+            }
+
+            if (SellIn < 0)
+            {
+                if (Name != "Aged Brie")
+                {
+                    if (Name != "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (Quality > 0)
+                        {
+                            if (Name != "Sulfuras, Hand of Ragnaros")
+                            {
+                                Quality = Quality - 1;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Quality = Quality - Quality;
+                    }
+                }
+                else
+                {
+                    if (Quality < 50)
+                    {
+                        Quality = Quality + 1;
+                    }
+                }
+            }
+        }
+    }
+}
